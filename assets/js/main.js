8ba058cd4604d6a420205ab980674e0affb87eff
Victor Soto Barba <vsbarba@gmail.com>
2014-03-21 16:55:16 +0800

    Updates

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
  $.firefly({
-     images : ['http:/vsbarba.github.io/iamdash2/assets/img/firefly/1.jpg', 'http:/vsbarba.github.io/iamdash2/assets/img/firefly/2.jpg'], //You can change images
+     images : ['//vsbarba.github.io/iamdash2/assets/img/firefly/1.jpg', '//vsbarba.github.io/iamdash2/assets/img/firefly/2.jpg'], //You can change images
      total : 65 // You can edit the number of flies
  });
 });
