5ef7cb5dba9d003b6388746362fccc895ecbaee1
Victor Soto Barba <vsbarba@gmail.com>
2014-03-21 16:46:28 +0800

    Updates

---
+++ assets/js/main.js
@@ -0,0 +1,6 @@
+$(document).ready(function() {
+ $.firefly({
+     images : ['/assets/img/firefly/1.jpg', '/assets/img/firefly/2.jpg'], //You can change images
+     total : 65 // You can edit the number of flies
+ });
+});
