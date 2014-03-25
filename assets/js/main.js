1b47fef8f090e6ceb95ef654873e3191853a005a
Victor Soto Barba <vsbarba@gmail.com>
2014-03-25 17:06:15 +0800

    Updates

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,19 @@
-$(document).ready(function() {
- $.firefly({
-     images : ['//vsbarba.github.io/iamdash2/assets/img/firefly/1.jpg', '//vsbarba.github.io/iamdash2/assets/img/firefly/2.jpg'], //You can change images
-     total : 65 // You can edit the number of flies
- });
+$(document).ready(function () {
+  $.firefly({
+    images: ['//vsbarba.github.io/iamdash2/assets/img/firefly/1.jpg', '//vsbarba.github.io/iamdash2/assets/img/firefly/2.jpg'], //You can change images
+    total: 65 // You can edit the number of flies
+  });
+
+  if (window.addEventListener) {  
+    var kkeys = [],
+      konami = "38";  
+    window.addEventListener("keydown", function (e) {    
+      kkeys.push(e.keyCode);    
+      if (kkeys.toString().indexOf(konami) >= 0) {
+        // User entered Konami Code, do something cool!!!
+        alert('Fuck you bitches!!');
+      }            
+      kkeys = [];  
+    }, true);
+  }
 });
